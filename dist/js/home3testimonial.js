b79058fe71d88c7c59e52e629a57d5b8eac0d4cb
lans <139159115+772nacho@users.noreply.github.com>
2024-09-06 23:07:34 +0800

    master to santiago

---
+++ dist/js/home3testimonial.js
@@ -0,0 +1,11 @@
+var slider = tns({
+    container: '.home-3__testimonialSlider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    mouseDrag: true,
+    autoplayButtonOutput: false,
+    navPosition: 'bottom',
+    autoplayTimeout: 10000
+  });
